34252f3169111ff6470b6bde2bb33ca4b7b81d13
Javodg <javodg@gmail.com>
2016-05-05 12:40:25 -0500

    Inicio de UI

---
+++ imports/ui/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// Componente de navbar
+export default class Navbar extends Component {
+
+
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+        Navegacion
+      </nav>
+    );
+  }
+}
